0fd128294bbfab9b21b85904f4879e6ad7923f97
rockyxshen <rockyxshen@gmail.com>
2014-03-29 13:38:14 -0400

    refactor

--- demoproject/apps/djfacet/views.py
+++ demoproject/apps/djfacet/views.py
@@ -10,9 +10,6 @@ from djfacet.cache_manager import *
 from djfacet.load_all import *
 from djfacet.fb_utils.utils import *
 from djfacet.fb_utils.template import render_block_to_string
-from demoproject.apps.djfacet.facetedmanager import access_fmglobal
-from demoproject.apps.djfacet.fb_utils.utils import create_queryUrlStub, djfacetlog
-
 
 def home(request):
     """
@@ -24,7 +21,6 @@ def home(request):
     it tries to remove the wrong ones, recompose the url and issue a redirect command.
     In this case a HttpResponseRedirect is returned, not a tuple, so an if statement handles that situation.
     """
-    djfacetlog("home view start to dispatch...")
     query_filtersUrl = request.GET.getlist('filter')
     item = request.GET.get('item', None)
     resulttype = validate_ResType(request.GET.get('resulttype', None))
@@ -33,14 +29,11 @@ def home(request):
         # redirect to the all-facets page
         return redirect("allfacets/?resulttype=%s" % resulttype)
     elif item:
-        djfacetlog("Single item dispatched! %s" % item)
-        # contains only an item, redirect to the single page.
+        # contains an item, redirect to the single page.
         results = single_item(request, item)
     else:
-        djfacetlog("classic search dispatched!")
         # it's classic search page
         results = search_page(request)
-
     # finally..
     if type(results) == type(('a tuple',)):
         template, context = results
@@ -53,14 +46,14 @@ def home(request):
 
 def single_item(request, item):
     """
-    Handles single item query request.
-
     """
     FM_GLOBAL = access_fmglobal()
     page, resulttype, ordering, query_filtersUrl, query_filtersBuffer, activeIDs, item, totitems, showonly_subs, history = __extractGETparams(
         request)
+    single_item_template, table_template = None, None
     redirect_flag, query_filtersUrl_Clean = __validateQueryFilters(resulttype, query_filtersUrl, FM_GLOBAL)
     if redirect_flag:
+        djfacetlog("FacetViews>> Redirecting; the url contained  invalid filter values!", True)
         newurl_stub = create_queryUrlStub(query_filtersUrl_Clean)
         newurl_stub = "%s?resulttype=%s%s" % (request.path, resulttype, newurl_stub)
         return redirect(newurl_stub)
